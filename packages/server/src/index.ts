31392326234271298f7743e46e326e68186d7c8a
Smruti Ranjan Badatya <smrutiranjanbadatya2@gmail.com>
2025-10-23 19:25:50 +0530

    update/server: Change Structure for Vercel Deployment

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,15 +1,53 @@
+import express, { NextFunction, Request, Response } from "express";
+import vhost from "vhost";
+import { corsFix } from "./libs/cors";
+import { requestBodyParser } from "./libs/bodyParser";
+import cookieParser from "./libs/cookieParser";
+
+import { mainRouter } from "./routes";
+import httpLogger from "./libs/morgan";
+
+import { multiTenantPostRouter } from "./domains/post/router";
+import collectHostDetails from "./middlewares/collectHostDetails";
 import { connectDatabase } from "./configs/db";
-import app from "./server";
-import dotenv from "dotenv";
-import { cronJobs } from "./jobs/cron";
 import logger from "./configs/logger";
+import { cronJobs } from "./jobs/cron";
+import dotenv from "dotenv";
 
 dotenv.config();
+
+const app = express();
+
+app.use(httpLogger);
+app.use(corsFix);
+app.use(requestBodyParser);
+app.use(cookieParser);
+app.use(collectHostDetails);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  const host = req.headers.host || "";
+  
+  if (host.startsWith("localhost") || host === "cms-server-agilecoder.vercel.app") {
+    mainRouter(req, res, next);
+  } else {
+    multiTenantPostRouter(req, res, next);
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("The Server is Running Fine 🚀");
+});
+
 const PORT = Number(process.env.PORT) || 3001;
 
 connectDatabase(() => {
-  app.listen(PORT, () => {
-    logger.info(`Server is running at http://localhost:${PORT} 🚀`);
-    cronJobs();
-  });
+  if (process.env.VERCEL !== "1") {
+    app.listen(PORT, () => {
+      logger.info(`Server is running at http://localhost:${PORT} 🚀`);
+    });
+  }
+  
+  cronJobs();
 });
+
+export default app;
